9a131ba81f15efde137d2f9473d0e081b31d6a4d
Mitch Kahn <mitch@appdelegates.com>
2015-01-29 21:22:33 -0700

    Completely changed the data model. See my email.

--- js/cellController.js
+++ js/cellController.js
@@ -6,110 +6,142 @@
 app.controller("cellController", function($scope, $http) {
 
     $scope.myModel = {message: ""};
-    $scope.realName= "Treb Ryan";
-    $scope.emailAdress="trebryan@gmail.com";
-    $scope.nickName="The Man!";
-    $scope.initials = "RJR";
+
+    //MAK moved player to an object because of Angular's issues with simple content types like strings
+    //and it's syntactically cleaner.
+    $scope.player = {
+        realName: "Treb Ryan",
+        emailAddress: "trebryan@gmail.com",
+        nickName: "The Man!",
+        initials: "RJR",
+        totalPicks: 0,
+        payNow: 0,
+        playersCells: []
+    }
+
     $scope.pickFile = [];
-    $scope.totalPicks = 0;
-    $scope.paidPicks= 0;
-    $scope.unpaidPicks=0;
+
     $scope.pickPrice=10;
-    $scope.payNow=0;
 
-    //$http.get('data/picks.json').success(function(data) {
-    //    $scope.pickFile = data;
-    //    console.log($scope.pickFile.length + " is the length of the pickFile after loading");
-    //});
+    //Create an empty playingField. This is populated via the GET below
+    $scope.playingField = new Array(10);
 
-    var promise = $http.get('data/picks.json');
 
-    promise.then( function(payload) {
-        $scope.pickFile = payload.data;
-    });
+
 
     var getCost = function(){
         $scope.unpaidPicks = $scope.totalPicks - $scope.paidPicks;
         $scope.payNow = $scope.unpaidPicks * $scope.pickPrice;
     }
 
+    var PickCell = function(screenRow, screenColumn){
 
-    $scope.load = function(cell) {
-        promise.then( function() {
-            for (var record in $scope.pickFile) {
-                var currentCell = "" + cell.screenRow + cell.screenColumn;
-                //console.log(currentCell);
-                console.log($scope.pickFile[record].CellID);
-                if (currentCell === $scope.pickFile[record].CellID) {
-                    cell.initials = $scope.pickFile[record].initials;
-                    $scope.totalPicks++;
-                    cell.previouslyUnpicked = true;
+        this.screenRow = screenRow;
+        this.screenColumn = screenColumn;
+        this.scoreColumn = -1;
+        this.scoreRow = -1;
+        this.initials = "";
+        this.paid = false;
+
+        this.picked = function(){
+            if (this.paid){
+                //TODO toast message
+                console.log("Cannot change a paid cell");
+            } else if (this.initials == ""){
+                this.initials = $scope.player.initials;
+                $scope.player.playersCells.push(this);
+            } else if ( this.initials == $scope.player.initials){
+                this.initials = ""; //unpick, but only if picked by same user
+                var idx = $scope.player.playersCells.indexOf(this);
+                if (idx>-1){
+                    $scope.player.playersCells.splice(idx,1);//remove that cell
                 }
-            }
-            getCost();
-        })
-        };
 
-
-
-
-
-        var PickCell = function(screenRow, screenColumn){
-
-            this.screenRow = screenRow;
-            this.screenColumn = screenColumn;
-            this.initials = "";
-            this.previouslyUnpicked = true;
-            this.currentlyUnpicked = true;
-
-            //this.UnpickedBy = function(initials){
-            //    console.log("Unpicked by: "+initials);
-            //    this.initials = initials;
-            //};
-            //
-            //this.unPick = function(){
-            //    this.intials = undefined;
-            //};
-
-
-        };
-
-        $scope.playingField = new Array(10);
-
-        for (var row=0; row<10; row++){
-            $scope.playingField[row] = new Array(10);
-            for (var col=0; col<10; col++){
-                $scope.playingField[row][col] = new PickCell(row, col);
-                //$scope.playingField[row][col].UnpickedBy(""+row+col);
+            } else {
+                //TODO "Nice try" message
+                console.log("Bzzzzz. You can't change someone else's shit, homey");
             }
         }
 
-        console.log("Field done");
+    };
 
-        $scope.selected = function(cell) {
-            console.log(cell.previouslyUnpicked);
-            if (cell.previouslyUnpicked) {
-                    var cellID = cell.screenRow.toString() + cell.screenColumn.toString();
-                console.log(cellID);
-                if (cell.currentlyUnpicked) {
-                    cell.initials = $scope.initials;
-                    $http.post('superbowl_pick.php', {CellID: cellID, initials: cell.initials});
-                    $scope.totalPicks++;
-                    getCost();
-                    cell.currentlyUnpicked = false;
-                }
-                else {
-                    cell.initials = undefined;
-                    $http.post('superbowl_unpick.php', {CellID: cellID, initials: cell.initials});
-                    $scope.totalPicks--;
-                    getCost();
-                    cell.currentlyUnpicked = true;
-                }
-
-                $scope.myModel.message = "Cell [" + cellID + "] "+cell.currentlyUnpicked;
-            }
+    for (var row=0; row<10; row++){
+        $scope.playingField[row] = new Array(10);
+        for (var col=0; col<10; col++){
+            $scope.playingField[row][col] = new PickCell(row, col);
         }
+    }
 
-    });
+    function processPickFile(){
+        $scope.pickFile.forEach(function(line){
+
+            line.forEach(function(record){
+
+                var row = record.screenRow;
+                var col = record.screenCol;
+
+                //Select the right visual cell from the array of playingField
+                var currentCell = $scope.playingField[row][col];
+
+                //Fill it with the saved data
+                currentCell.initials = record.initials;
+                currentCell.paid = record.paid;
+
+                //Put all the player's cells in a handy array
+                if (currentCell.initials==$scope.player.initials){
+                    $scope.playersCells.push(currentCell);
+                }
+
+            });
+
+         });
+
+    }
+
+    function pushFieldToServer(){
+        $http.post('superbowl_save_json.php', $scope.playingField).then(
+            function(data){
+                console.log("Saved field: "+data);
+            },
+            function(err){
+                console.log("Error saving field: "+data);
+            }
+        );
+    }
+
+    $http.get('data/picksfield.json').then(
+        function(payload) {
+            $scope.pickFile = payload.data;
+            processPickFile();
+        },
+        function(err){
+            //TODO implement either ngToast or similar to issue error message
+            console.log("Error getting picks.json!");
+            console.log(err);
+
+            //A missing file sometimes gives a weird err.status of 0 and not 404.
+            //TODO this requires some understanding, though accessing files directly wouldn't be the normal way to do this
+            if (err.status=404 || err.status==0){
+                //If there's no file, push a blank slate
+                pushFieldToServer();
+            }
+        });
+
+    $scope.getOwed = function(){
+
+        var total=0;
+
+        $scope.player.playersCells.forEach(function(cell){
+
+            if (cell.paid==false){
+                total+=$scope.pickPrice;
+            }
+        });
+
+        return total;
+
+    }
+
+});
 
 
